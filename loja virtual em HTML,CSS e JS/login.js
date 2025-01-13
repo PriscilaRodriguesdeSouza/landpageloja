05f17af7f6fc717885a9eac0eb1b31279a90514c
PriscilaRodriguesdeSouza <priscilasouzarodrigues@hotmail.com>
2025-01-13 08:46:04 -0300

    Add files via upload

---
+++ loja virtual em HTML,CSS e JS/login.js
@@ -0,0 +1,26 @@
+var menuitens = document.getElementById("menuitens");
+menuitens.style.maxHeight = "0px";
+
+function menucelular(){
+    if(menuitens.style.maxHeight == "0px"){
+        menuitens.style.maxHeight = "200px";
+    }else{
+        menuitens.style.maxHeight = "0px";
+    }
+}
+
+var EntrarPainel = document.getElementById("EntrarPainel");
+var CadastroSite = document.getElementById("CadastroSite");
+var Indicador = document.getElementById("Indicador");
+
+function Cadastro(){
+    CadastroSite.style.transform = "translateX(0px)";
+    EntrarPainel.style.transform = "translateX(0px)";
+    Indicador.style.transform = "translateX(100px)";
+}
+
+function Entrar(){
+    CadastroSite.style.transform = "translateX(300px)";
+    EntrarPainel.style.transform = "translateX(300px)";
+    Indicador.style.transform = "translateX(0px)";
+}
